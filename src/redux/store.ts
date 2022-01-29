4bfe6557bd59c6321682fb90bd606f27e4193da5
MATSUDA Takashi <tmatz.dev@gmail.com>
2022-01-29 14:57:21 +0900

    feat: add epic middleware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { createEpicMiddleware } from "redux-observable";
+
+const epicMiddleware = createEpicMiddleware();
 
 export const store = configureStore({
   reducer: {},
+  middleware: (getDefaultMiddlewares) => [
+    ...getDefaultMiddlewares({}),
+    epicMiddleware,
+  ],
 });
 
 export type AppDispatch = typeof store.dispatch;
