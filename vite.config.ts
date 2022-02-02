d86919a94385a8f455675855f974ecff8d7b65bb
MATSUDA Takashi <tmatz.dev@gmail.com>
2022-02-02 23:50:33 +0900

    config: npm run build:analize

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -7,4 +8,29 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {},
+  build: {
+    rollupOptions: {
+      plugins: [
+        visualizer({
+          gzipSize: true,
+          brotliSize: true,
+        }),
+      ],
+      output: {
+        manualChunks: {
+          unified: [
+            "rehype",
+            "rehype-raw",
+            "rehype-react",
+            "rehype-sanitize",
+            "remark",
+            "remark-gfm",
+            "remark-rehype",
+            "unified",
+            "unist-util-inspect",
+          ],
+        },
+      },
+    },
+  },
 });
