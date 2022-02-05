6018ca5990d36588591c87bc782323214029e5c9
MATSUDA Takashi <tmatz.dev@gmail.com>
2022-02-05 14:58:04 +0900

    config: absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
-import path from "path";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
   test: {},
 });
