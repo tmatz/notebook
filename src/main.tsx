2daaf1053bdf4a45c5a389456a42bedc7701db1d
MATSUDA Takashi <tmatz.dev@gmail.com>
2022-02-05 12:10:51 +0900

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import App from "~/components/App";
 import { store } from "~/redux/store";
 import "./index.css";
 
+const basename = import.meta.env.BASE_URL;
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter basename={basename}>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
