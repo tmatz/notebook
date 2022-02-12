3519dc90c293b127d8e2f23f114bba20d1e213ca
MATSUDA Takashi <tmatz.dev@gmail.com>
2022-02-12 23:36:36 +0900

    config: tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
+  syntax: "postcss-scss",
   plugins: {
+    "tailwindcss/nesting": {},
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content: [],
+  content: ["src/**/*.tsx"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
